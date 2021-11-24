260e813b8ba4936673483b6b1344da1f0ff6e4c3
Jonathan Haigh <jonathanhaigh0@gmail.com>
2021-11-24 11:32:59 +0000

    updated predict script

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for filename in data/CW/data/test/image/*; do
-  python predict.py -i data/CW/data/test/image/${filename##*/} -o data/CW/data/test/mask/${filename##*/} --model checkpoints/checkpoint_epoch5.pth
+for filename in data/test/image/*; do
+  clipped_name=${filename##*/}
+  python predict.py -i data/test/image/$clipped_name -o data/test/mask/${clipped_name%.*}_mask.png --model checkpoints/checkpoint_epoch5.pth
 done
